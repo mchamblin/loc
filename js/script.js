8a22bd8ade12efea01f4c247d11fb3f537d02db5
Matt Chamblin <31355195+mchamblin-turner@users.noreply.github.com>
2024-01-12 14:04:15 -0500

    add sentence generator

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('.list-group-item').forEach((item) => {
+    item.addEventListener('click', function () {
+      // Remove 'selected' class from all items in the same accordion
+      this.closest('.accordion')
+        .querySelectorAll('.list-group-item')
+        .forEach((el) => el.classList.remove('selected'));
+      // Add 'selected' class to the clicked item
+      this.classList.add('selected');
+    });
+  });
+});
+
 document.addEventListener('DOMContentLoaded', () => {
   var accordions = document.querySelectorAll('.accordion');
 
@@ -26,5 +39,25 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelector('.btn-primary').addEventListener('click', function () {
+    const observation = document.querySelector('#observation-input').value;
+    console.log(`observation`, observation);
+
+    const request = document.querySelector('#request-input').value;
+    console.log(`request`, request);
+
+    const selectedFeelingElement = document.querySelector('#accordionFeelings .selected');
+    const selectedNeedElement = document.querySelector('#accordionNeeds .selected');
+
+    const feeling = selectedFeelingElement ? selectedFeelingElement.textContent : '';
+    const need = selectedNeedElement ? selectedNeedElement.textContent : '';
+
+    const output = `When <strong>${observation}</strong>...<br>\u00A0\u00A0\u00A0\u00A0I feel <strong>${feeling}</strong><br>\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0I need <strong>${need}</strong>.<br><br>Would you be willing to <strong>${request}</strong>?`;
+
+    document.querySelector('#outputModal .modal-body').innerHTML = output;
   });
 });
