91aa64f1bc72e7a0818bdf9be7c26a43f4b7bd79
Matt Chamblin <31355195+mchamblin-turner@users.noreply.github.com>
2024-01-12 09:35:54 -0500

    update style.css

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var accordions = document.querySelectorAll('.accordion');
-  
-    accordions.forEach(function (accordion) {
-      var buttons = accordion.querySelectorAll('.accordion-button');
-      var currentSticky = null;
-  
-      buttons.forEach(function (button) {
-        button.addEventListener('click', function () {
-          var accordionHeader = button.closest('.accordion-header');
-  
-          // If there is a currently sticky accordion, remove the sticky class
-          if (currentSticky) {
-            currentSticky.classList.remove('sticky');
-          }
-  
-          // If the clicked button is not collapsed, make its accordion sticky
-          if (!button.classList.contains('collapsed')) {
-            accordionHeader.classList.add('sticky');
-            currentSticky = accordionHeader;
-          }
-        });
+document.addEventListener('DOMContentLoaded', () => {
+  var accordions = document.querySelectorAll('.accordion');
+
+  accordions.forEach((accordion) => {
+    var buttons = accordion.querySelectorAll('.accordion-button');
+    var currentSticky = null;
+
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        var accordionHeader = button.closest('.accordion-header');
+
+        // If there is a currently sticky accordion, remove the sticky class
+        if (currentSticky) {
+          currentSticky.classList.remove('sticky');
+        }
+
+        // If the clicked button is not collapsed, make its accordion sticky
+        if (!button.classList.contains('collapsed')) {
+          accordionHeader.classList.add('sticky');
+          currentSticky = accordionHeader;
+          // Remove sticky class from parent .accordion
+          accordion.classList.remove('sticky');
+        } else {
+          // If the clicked button is collapsed, add sticky class to parent .accordion
+          accordion.classList.add('sticky');
+        }
       });
     });
   });
-  
+});
